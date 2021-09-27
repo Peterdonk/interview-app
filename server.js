c66866e92d275485c2e2f544da031a9a4fc8bc67
PeterDonk <peterdonk17@gmail.com>
2021-09-27 12:49:15 +0000

    Updated Packages

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
